db8e60e349f52a3749747be0262abe75ced9ae31
Blakedoghyian <xblakedx@gmail.com>
2021-12-20 15:02:08 -0600

    Added initial questions to test index script and interpolating responses with template literals

--- Develop/index.js
+++ Develop/index.js
@@ -3,13 +3,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const promptUser = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the project name? (Required)',
+            // use validate to make sure user provides a response before continuing
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project name to continue!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a description of your project.(Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description to continue!');
+                    return false;
+                }
+            }
+        }
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log ('README successfully generated. Head over to README.md to check it out!');
+        }
+    })
+};
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // prompt user with questions
+    inquirer.prompt(promptUser)
+    // then run writetofile function to generate file filled in with new data
+    .then(function(data){
+        writeToFile( "README.md" , generateMarkdown(data) );
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,6 +14,25 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  # Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  ## Installation 
+
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ### Questions
+
+
 `;
 }
 
